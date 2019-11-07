bc18d41ac764ec32f2115430e71ffb382165d8ec
valentina.tikko <valentina.tikko@B111-Dozent.BK.local>
2019-11-07 15:43:35 +0100

    ein Link zum Mergen

--- Game/src/game/main/Game.java
+++ Game/src/game/main/Game.java
@@ -2,6 +2,7 @@ package game.main;
 
 import game.GameFrame;
 
+//https://allaboutmynonexistedworld.wordpress.com/2014/02/05/eclipse-git-merging-branch-to-master/
 public class Game {
 
 	public static void main(String[] args) {
